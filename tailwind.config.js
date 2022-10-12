d6b830dfa5e4b8fbe97f85743aa36d1a2ebedb09
Jordan Bowman <desk@jrdnbwmn.com>
2022-10-12 16:56:59 -0400

    Set things up

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,12 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    theme: {
-      extend: {},
+    fontFamily: {
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Inter'],
+      'body': ['Inter']
     },
   },
   plugins: []
