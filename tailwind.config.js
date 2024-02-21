79beeb1e1de2843f68cc851805d7a71ba739233e
Jordan Bowman <desk@jrdnbwmn.com>
2024-02-20 19:15:28 -0500

    Setting up Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+	'./_drafts/**/*.html',
+	'./_includes/**/*.html',
+	'./_layouts/**/*.html',
+	'./_posts/*.md',
+	'./_tools/*.md',
+	'./*.md',
+	'./*.html',
+  ],
+  theme: {
+	fontFamily: {
+	  'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+	  'serif': ['ui-serif', 'Georgia'],
+	  'mono': ['ui-monospace', 'SFMono-Regular'],
+	  'display': ['Inter'],
+	  'body': ['Inter']
+	},
+  },
+  plugins: [
+	  require('@tailwindcss/typography'),
+  ],
+}
